d9f7164addf3993931e239d04cd3e40eb5654a16
Atul Verma <atulverma82@yahoo.com>
2013-10-20 15:24:17 +0530

    adding admin check for user list

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,11 @@
 class UserController < ApplicationController
-  def index
+
+ def index
+    if !current_user.is_admin?
+      flash[:error] = 'Not authorized'
+      redirect_to root_path
+    end
+
     @users = User.all
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,23 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
-  has_many :roles
+  belongs_to :role
+  before_create :set_default_role
+
+
+  def is_admin?
+    self.role.name == 'admin'
+  end
+  private
+  def set_default_role
+    self.role ||= Role.find_by_name('reporter')
+  end
+
+
 end
