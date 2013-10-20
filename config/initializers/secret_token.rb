331dc7b29be51cb9043701886ed8da1a2e544778
Atul Verma <atulverma82@yahoo.com>
2013-10-20 13:44:42 +0530

    initial commit with rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContentPublishing::Application.config.secret_token = 'e6c57dd6f0551d7e7a9c116aa4612aed55e4514bc285779a85c4ed9f1da5677856e345e17a90586abcb06850552c875705fc18addc7527f640a0595af021ad5d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ContentPublishing::Application.config.session_store :cookie_store, key: '_content_publishing_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ContentPublishing::Application.config.session_store :active_record_store
