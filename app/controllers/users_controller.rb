c691d01abfa17520c60c4604dabeada973a6d350
Atul Verma <atulverma82@yahoo.com>
2013-11-05 20:46:50 +0530

    edit and update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
  authorize_resource
- before_filter :user_exists? , only: [:edit]
+ before_filter :user_exists? , only: [:edit, :update]
 
  def index
     if !current_user.is_admin?
@@ -13,9 +13,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    role = params[:user].delete(:role)
+    role = get_role_from_params
     @user = User.new params[:user]
-    @user.role = Role.find_by_name(role) if role
+    @user.role = role
 
     if @user.save
       flash[:success] = I18n.t('articles.successfully_created')
@@ -33,7 +33,29 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    params[:user].delete(:email)
+    @user = User.find(params[:id])
+    @user.role = get_role_from_params
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      flash[:success] = I18n.t('user.successfully_updated')
+      redirect_to users_path
+    else
+      flash[:error] = I18n.t('user.update_error')
+      render action: :edit
+    end
+  end
+
   private
+
+  def get_role_from_params
+    role = params[:user].delete(:role)
+    Role.find_by_name(role) if role
+  end
 
   def user_exists?
     unless User.exists?(params[:id])
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -59,6 +59,39 @@ describe  UsersController do
       response.should render_template 'errors/details'
     end
   end
-end
 
+  context :update do
+
+    before :each do
+      login_admin
+    end
+
+    it 'should update user' do
+      user = FactoryGirl.create(:user, email: 'foo@baz.com')
+      old_password = user.encrypted_password
+
+      request_params = {id: user.id, user: {role: 'editor', password: 'password', password_confirmation: 'password', email: 'bas@bas.com'}}
+      put :update, request_params
+      response.should redirect_to users_path
+
+      user.reload
+      assigns(:user).email.should == 'foo@baz.com'
+      user.email.should == 'foo@baz.com'
+      assigns(:user).id.should == user.id
+      user.encrypted_password.should_not == old_password
+      assigns(:user).encrypted_password.should == user.encrypted_password
+      flash[:success].should == I18n.t('user.successfully_updated')
+    end
+
+    it 'should handle errors' do
+      user = FactoryGirl.create(:user, email: 'foo@baz.com')
+      request_params = {id: user.id, user: {password: ''}}
+      put :update, request_params
+
+      response.should render_template :edit
+      flash[:error].should == I18n.t('user.update_error')
+    end
+  end
+
+end
 
