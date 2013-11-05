65c69942d0935b535fccbf423d5c9f72974a789f
Atul Verma <atulverma82@yahoo.com>
2013-11-05 12:47:16 +0530

    rendering menus by role

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+
+  def menu_for_role
+    if user_signed_in?
+      get_menu_items[current_user.role.name.to_sym][:view]
+    else
+      get_menu_items[:guest][:view]
+    end
+  end
+
+  def action_menu_for_role
+    if user_signed_in?
+      get_menu_items[current_user.role.name.to_sym][:action]
+    else
+      get_menu_items[:guest][:action]
+    end
+  end
+
+  def get_menu_items
+    @@menu ||= {
+      guest: { view: {"Articles" => articles_path}, action: {}} ,
+      reporter: {view:{"Home" => root_path, "Articles" => articles_path}, action: { "New Article" => new_article_path}},
+      editor: {view:{"Home" => root_path, "Articles" => articles_path}, action: {"New Article" => new_article_path}},
+      admin: {view:{"Home" => root_path, "Articles" => articles_path, "Users" => users_path}, action: {"New Article" => new_article_path, "New User" => new_user_path}}
+    }
+  end
 end
